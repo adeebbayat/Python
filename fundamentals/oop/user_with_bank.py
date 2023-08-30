0949e37be60c70e43f3c66d6fb818b8a50b14e81
Adeeb Bayat <adeebbayat@yahoo.com>
2023-08-30 13:52:30 -0700

    Initial Commit

--- fundamentals/oop/user_with_bank.py
+++ fundamentals/oop/user_with_bank.py
@@ -1,17 +1,7 @@
 class BankAccount:
-
-    all_accounts = []
-
-
     def __init__(self,int_rate, balance = 0):
         self.int_rate = int_rate
         self.balance = balance
-        BankAccount.all_accounts.append(self)
-        
-    @classmethod
-    def print_all(cls):
-        for account in cls.all_accounts:
-            account.display_account_info()
 
     
     def deposit(self,amount):
@@ -34,14 +24,29 @@ class BankAccount:
     def yield_interest(self):
         if (self.balance > 0):
             self.balance += self.balance*self.int_rate
-        return self
+        return self 
     
     
 
-account1 = BankAccount(0.05,10000)
-account2 = BankAccount(0.02,200000)
+class User:
+    def __init__(self,name,email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate=0.02,balance=0)
 
-account1.deposit(100).deposit(50).deposit(300).withdraw(200).yield_interest()
-account2.deposit(1000).deposit(2000).withdraw(300).withdraw(200).withdraw(100).withdraw(1000).yield_interest()
+    def make_deposit(self,amount):
+        self.account.deposit(amount)
+    
+    def display_user_balance(self):
+        print(f"Balance: ${self.account.balance}")
 
-BankAccount.print_all()
+
+
+user1 = User("Brad","brad@yahoo.com")
+user1.make_deposit(200)
+user1.display_user_balance()
+
+user2 = User("Chad","chad@yahoo.com")
+user2.make_deposit(200)
+user2.display_user_balance()
+
