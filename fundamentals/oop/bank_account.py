cc66273bab08e0a6986ca31ce6634cde32d68e2c
Adeeb Bayat <adeebbayat@yahoo.com>
2023-08-30 12:35:46 -0700

    Initial Commit

---
+++ fundamentals/oop/bank_account.py
@@ -0,0 +1,33 @@
+class BankAccount:
+    def __init__(self,int_rate, balance = 0):
+        self.int_rate = int_rate
+        self.balance = balance
+
+    
+    def deposit(self,amount):
+        self.balance += amount
+        return self
+    
+    def withdraw(self,amount):
+        if(self.balance > amount):
+            self.balance -= amount
+            return self
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+            return self
+    
+    def display_account_info(self):
+        print(f"Balance: ${self.balance}")
+        return self
+    
+    def yield_interest(self):
+        if (self.balance > 0):
+            self.balance += self.balance*self.int_rate
+        return self
+
+account1 = BankAccount(0.05)
+account2 = BankAccount(0.02)
+
+account1.deposit(100).deposit(50).deposit(300).withdraw(200).yield_interest().display_account_info()
+account2.deposit(1000).deposit(2000).withdraw(300).withdraw(200).withdraw(100).withdraw(1000).yield_interest().display_account_info()
