38c6d7aade525c947964cdfd58688feb64b341f3
Adeeb Bayat <adeebbayat@yahoo.com>
2023-09-05 17:54:44 -0700

    Initial Commit

--- dojo_survey/server.py
+++ dojo_survey/server.py
@@ -1,22 +1,31 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect,session
 
 app = Flask(__name__)
 app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
 
 
 
-@app.route('/')
+@app.route('/',methods=["POST"])
 def index():
 
     return render_template("index.html")
 
-@app.route('/process' method="post")
-def form_submit(textBox1,Locations,Languages,textBox):
+@app.route('/process', methods=['POST'])
+def form_submit():
     print("Got Post Info")
-    print(request.form)
+    session["name"] = request.form["textBox1"]
+    session["location"] = request.form["Locations"]
+    session["language"] = request.form["Language"]
+    session["comment"] = request.form["textBox"]
 
+    return redirect('/result')
 
-    return redirect('/')
+@app.route('/result')
+def form_result():
+    return render_template("result.html", name_on_template=session["name"],
+    location_on_template=session["location"],language_on_template=session["language"],
+    comment_on_template=session["comment"])
+
 
 
 if __name__ == "__main__":
